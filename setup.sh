92491a91ca845f37e9c95444774c4f6531d667e8
Will Tran <dutran@zendesk.com>
2018-04-01 16:35:56 +0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# TODO: check file exist & emit error before continuing
+ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/.tmux.conf.local $HOME/.tmux.conf.local
+ln -s $(pwd)/.vimrc $HOME/.vimrc

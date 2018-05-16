96519fb548db68927c1848036cf11754ab1a0853
Will Tran <dutran@zendesk.com>
2018-05-16 23:20:24 +0800

    Update file structure and setup scripts

--- setup.sh
+++
@@ -1,4 +0,0 @@
-# TODO: check file exist & emit error before continuing
-ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/.tmux.conf.local $HOME/.tmux.conf.local
-ln -s $(pwd)/.vimrc $HOME/.vimrc

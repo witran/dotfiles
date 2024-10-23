2edd51f24c517ef3f1da9ce943fc88c2a1c730c3
William <witrandev@gmail.com>
2024-10-23 19:52:48 +0700

    Fix file name

--- get-vim.sh
+++ get-vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt-get update
 sudo apt-get install vim
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 ln -sf $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -sf $(pwd)/tmux.conf.local $HOME/.tmux.conf.local
 ln -sf $(pwd)/vimrc $HOME/.vimrc
-ln -sf $(pwd)/aliases $HOME/.aliases
-ln -sf $(pwd)/ps $HOME/.ps
+ln -sf $(pwd)/aliases.sh $HOME/.aliases.sh
+ln -sf $(pwd)/ps.sh $HOME/.ps.sh
 ln -sf $(pwd)/gitconfig $HOME/.gitconfig
 
-source_cmd="source ~/.aliases && source ~/.ps"
+source_cmd="source ~/.aliases.sh && source ~/.ps.sh"
 bash_file_path="$HOME/.bashrc"
 grep -qxF "$source_cmd" $bash_file_path || echo $source_cmd >> $bash_file_path
